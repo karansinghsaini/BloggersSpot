745dfef6478f783f6f814b3220f09827c2fc88ce
Karan Singh Saini <karansingh7613@gmail.com>
2020-10-20 16:38:33 +0530

    changes in Api

---
+++ Updateprofile.js
@@ -0,0 +1,123 @@
+import React, {useState, useEffect} from 'react';
+import {Form, Button,Col,InputGroup} from 'react-bootstrap';
+import '../../css/profile/updateprofile.css';
+import {useSelector, useDispatch} from 'react-redux';
+import jwt from 'jsonwebtoken';
+import {GetUserProfile} from '../../redux/actions/user';
+import {UpdateProfile} from '../../redux/actions/profile';
+
+const Profile = () => {
+    const data = jwt.decode(localStorage.jwtToken);
+    const dispatch = useDispatch();
+    const user_data = useSelector( (state) => state.UserProfile.curr_user_data);
+
+    useEffect( () => {
+        dispatch(GetUserProfile(data.id));
+    }, []);
+
+    const[username,setUsername] = useState(user_data.username);
+    const[fullname, setFullname] = useState(user_data.fullname);
+    const[bio, setBio] = useState(user_data.bio);
+    const[gender, setGender] = useState(user_data.gender);
+    const[phone, setPhone] = useState(user_data.phone);
+    const[website, setWebsite] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const updated_data = {
+            "fullname": fullname,
+            "username": username,
+            "bio": bio,
+            "gender": gender,
+            "phone": phone,
+            "website": website
+        };
+        dispatch(UpdateProfile(data.id, updated_data));
+    };
+
+    return(
+        <div className='profile-div'>
+            <h3 className='profile-head'>Welcome To your Profile</h3>
+            <div className='profile-container'>
+                <Form onSubmit={handleSubmit}>
+                <Form.Row>
+                    <Form.Group as={Col} md="4" controlId="validationCustom01">
+                        <Form.Label>Full Name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            value={fullname}
+                            onChange = { (e) => setFullname(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                        <Form.Label>Username</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <Form.Control
+                            type="text"
+                            value={username}
+                            aria-describedby="inputGroupPrepend"
+                            onChange = { (e) => setUsername(e.target.value)}
+                            required
+                            />
+                        </InputGroup>
+                    </Form.Group>
+
+                    <Form.Group as={Col} md="4" controlId="validationWebsite">
+                    <Form.Label>Website</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={website}
+                        onChange = { (e) => setWebsite(e.target.value)}
+                    />
+                    </Form.Group>
+
+                    
+                </Form.Row>
+                <Form.Row>
+                    <Form.Group as={Col} md="6" controlId="validationCustom03">
+                        <Form.Label>Phone</Form.Label>
+                        <Form.Control 
+                        type="number" 
+                        value={phone} 
+                        onChange = { (e) => setPhone(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group as={Col} md="3" controlId="validationCustom05">
+                        <Form.Label>Gender</Form.Label>
+                        <Form.Control 
+                        type="text" 
+                        value={gender}
+                        required 
+                        onChange = { (e) => setGender(e.target.value)}
+                        />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Row>
+                    <InputGroup as={Col}>
+                        <InputGroup.Prepend>
+                        <InputGroup.Text>Bio</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control 
+                        as="textarea" 
+                        value={bio}
+                        aria-label="With textarea" 
+                        onChange={ (e) => setBio(e.target.value)} />
+                    </InputGroup>
+                </Form.Row><br />
+                    
+                    <Button type="submit">Update Profile</Button>
+                </Form>
+            </div>
+        </div>
+    );
+
+}
+
+export default (Profile);
---
+++ blogs.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const blogs = require('../models/blogs');
+const votes = require('../models/votes');
+//var _ = require('lodash');
+// module for creating JWT tokens.
+var jwt = require('jsonwebtoken');
+// importing our verifyToken function
+const verifyToken = require('./verifyToken');
+
+const route = express.Router();
+
+// secret key used while creating token.
+const secret = '53ddf1277aa9cce7f64fd176d566553322a86c139047a1d9c7a8e09c2500029ba167c9efba48fe49e9c81308f4d3c03c64016ad05478b3785432aea52ab5043a';
+
+route.post('/newblog', verifyToken, (req,res) => {
+    jwt.verify(req.token, secret, (err, authData) => {
+        if(err) {
+          res.sendStatus(403);
+        } else {
+            const date = new Date();
+            var blogModel = new blogs({
+                'title': req.body.title,
+                'content': req.body.content,
+                'likes': req.body.likes,
+                'author': authData.username,
+                'user_id': authData.id,
+                'date_created': date.toString()
+              });
+              blogModel.save(err => {
+                if (err) return res.json({
+                    success: false,
+                    error: err.message
+                });
+                return res.json({
+                    success: true,
+                    status: 200
+                });
+            });
+        }
+    });
+});
+
+route.get('/getBlogs', verifyToken, (req, res) => {
+
+    jwt.verify(req.token, secret, (err, authData) => {
+        if(err) {
+          res.sendStatus(403);
+        } else {
+            blogs.find({}).sort({date_created: -1}).exec(function(err,blogs_data){
+                if (err) return res.json({
+                    success: false,
+                    error: err.message
+                });
+                votes.find({})
+                .then( votes_data => {
+                    return res.json({
+                        blogs: blogs_data,
+                        votes: votes_data
+                    });
+                });
+            });
+        }
+    });
+});
+
+route.get('/getBlog/:id', verifyToken, (req, res) => {
+    jwt.verify(req.token, secret, (err, authData) => {
+        if(err) {
+          res.sendStatus(403);
+        } else {
+            blogs.findOne({_id: req.params.id}).exec(function(err,data){
+                if (err) return res.json({
+                    success: false,
+                    error: err.message
+                });
+                return res.json(data);
+            });
+        }
+    });
+});
+
+route.delete('/deleteBlogs/:id', verifyToken, function (req, res) {
+    
+
+    jwt.verify(req.token, secret, (err, authData) => {
+        if(err) {
+          res.sendStatus(403);
+        } else {
+            blogs.findByIdAndRemove({
+                "_id": req.params.id
+            }).then(function (blogs) {
+                res.send(blogs);
+            });
+        }
+    });
+});   
+
+
+
+route.put('/updateBlogs/:id', verifyToken, (req, res) => {
+    const update = req.body;
+
+    jwt.verify(req.token, secret, (err, authData) => {
+        if(err) {
+          res.sendStatus(403);
+        } else {
+            blogs.findByIdAndUpdate({ "_id": req.params.id }, update, err => {
+                if (err) return res.json({
+                    success: false,
+                    error: err
+                });
+                return res.json({
+                    success: true,
+                    authData
+                });
+            });
+        }
+    });
+});   
+
+module.exports = route;
