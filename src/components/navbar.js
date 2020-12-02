3fbd600c5ec67ee05c0cdd2370eec34d320fcae9
Karan Singh Saini <karansingh7613@gmail.com>
2020-12-02 15:31:24 +0530

    Made changes in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,12 +12,15 @@ const Menu = () => {
 
     return(
         <>
-            <Navbar sticky="top" bg="light" variant="light">
+            <Navbar sticky="top" bg="light" variant="light" expand="lg">
+            {isLoggedIn && <Navbar.Brand href="/home"><Image src={Logo}></Image></Navbar.Brand>}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto nav-font">
-                {isLoggedIn && <Nav.Link href="/home"><Image src={Logo}></Image></Nav.Link>}
+                {isLoggedIn && <Nav.Link href="/home">Home</Nav.Link>}
                 {isLoggedIn && <Nav.Link href="/new-blog">New Blog</Nav.Link>}
                 {isLoggedIn && <Nav.Link href="/authors">Find Authors</Nav.Link>}
-                {isLoggedIn && <NavDropdown title="Profile" id="collasible-nav-dropdown">
+                {isLoggedIn && <NavDropdown title={data.username} id="collasible-nav-dropdown">
                     <NavDropdown.Item href = {`/profile/${data.id}`}>Profile</NavDropdown.Item>
                     {/* <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
                     <NavDropdown.Item href="#">Something</NavDropdown.Item> */}
@@ -31,11 +34,12 @@ const Menu = () => {
                 {!isLoggedIn && <Nav.Link href="/register">Register</Nav.Link>}
                 </Nav>
     
+                
                 {isLoggedIn && <Form inline>
-                {/* <FormControl type="text" placeholder="Search Authors" className="mr-sm-2" />
-                <Button variant="outline-info">Search</Button>&nbsp;&nbsp; */}
-                Welcome {data.username}
+                    <FormControl type="text" placeholder="Search Authors" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>&nbsp;&nbsp;
                 </Form>}
+             </Navbar.Collapse>
             </Navbar>
         </>
     );
